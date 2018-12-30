aea1ae876fae9c73361dced5afb618de435bf20c
victor <victor@rainvic.com>
2018-12-31 06:58:43 +0800

    - create navigation - set navigation host - navigate to detail page - add navigation ktx

--- app/src/main/java/com/rainvic/kotlinandroidsample/MainActivity.kt
+++ app/src/main/java/com/rainvic/kotlinandroidsample/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.rainvic.kotlinandroidsample
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import com.rainvic.kotlinandroidsample.fragments.DetailPageFragment
+import com.rainvic.kotlinandroidsample.fragments.LandingPageFragment
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
